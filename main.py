1645ce16aa8cf1e71b50214e648b3606f8089f8a
Enevaldart <enevaldbravo80@gmail.com>
2023-03-28 08:18:15 +0300

    28th march version

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import threading
 import time
-from flask import Flask, render_template,  request, redirect, url_for, send_from_directory
+from flask import Flask, render_template,  request
 from werkzeug.utils import secure_filename
-import os, shutil
-from script import convert
-from speech import text_to_speech
-import pyttsx3
-from flask_cors import cross_origin
+import os
 from flask import send_file
-from flask import after_this_request
+import io
+import PyPDF2
+import pyttsx3
+import pdfplumber
 
 
 app = Flask(__name__)
@@ -36,7 +34,7 @@ def behind_the_making():
 def Terms_of_service():
     return render_template('terms.html')
 
-#make the following codes  comment and then test you r programagain
+#Analyse the target file to produce audio
 @app.route("/convert_pdf", methods=['GET', 'POST'])
 def convert_pdf():
     #request file through post method
@@ -44,59 +42,42 @@ def convert_pdf():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            timestr = time.strftime("%Y%m%d_%H%M%S")
 
-            save_location = os.path.join('uploads', filename)
-            save_location = save_location.replace(' ', '_')
-            save_location = save_location.replace('\\', '/')
-
-            #renaming of the file to prevent duplicates
-            save_location = save_location.replace('.pdf', '_listenToPdf' + timestr + '.pdf')
-            file.save(save_location)
-
-            #convert the file in the absolute path specified
-            convert(save_location)
-
-            #save the converted file in a directory
-            save_location = save_location.replace('pdf', 'mp3')
-            save_location = save_location.replace('uploads', 'downloads')
-            file_path = save_location
-            file_handle = open(file_path, 'r')
-            file_handle.close()
-
-            #downloading converted file|s
+            #convert the pdf file into audio
             try:
-                return send_file(file_path, as_attachment=True)
+                pdfReader = PyPDF2.PdfFileReader(file)
+                pages = pdfReader.numPages
+                finalText = ""
+                with pdfplumber.open(file) as pdf:
+                    for i in range(0, pages):
+                        page = pdf.pages[i]
+                        text = page.extract_text()
+                        finalText += text
+                        finalText = ' '.join(finalText.splitlines())
 
-            #Deleting outdated files
+                engine = pyttsx3.init()
+                voices = engine.getProperty('voices')
+                engine.setProperty('voice', voices[1].id)
+                engine.setProperty('rate', 170)
+
+                # synthesizing the text as audio using pyttsx3
+                audio_file = io.BytesIO()
+                engine.save_to_file(finalText, audio_file)
+                audio_file.seek(0)
+
+                engine.runAndWait()
+                timestr = time.strftime("%Y%m%d_%H%M%S")
+                download_file="ListenToPdf"+timestr+filename
+                base, ext = os.path.splitext(download_file)
+                download_file = base + '.' + 'mp3'
+                return send_file(audio_file, as_attachment=True, attachment_filename=download_file)
+
+            #finally if everything goes well and by chance the execution is terminated
             finally:
-                #delete in the downloads directory
-                folder = 'downloads'
-                for filename in os.listdir(folder):
-                    file_path = os.path.join(folder, filename)
-                    try:
-                        if os.path.isfile(file_path) or os.path.islink(file_path):
-                            os.unlink(file_path)
-                        elif os.path.isdir(file_path):
-                            shutil.rmtree(file_path)
-                    except Exception as e:
-                        print('failed to delete %s. Reason: %s' % (file_path, e))
-             #   print('Audio deleted!')
-                #delete in the uploads directory
-                folder2 = 'uploads'
-                for filename in os.listdir(folder2):
-                    file_path = os.path.join(folder2, filename)
-                    try:
-                        if os.path.isfile(file_path) or os.path.islink(file_path):
-                            os.unlink(file_path)
-                        elif os.path.isdir(file_path):
-                            shutil.rmtree(file_path)
-                    except Exception as e:
-                        print('failed to delete %s. Reason: %s' % (file_path, e))
-                print('Pdf deleted!')
+                print('Everything went smooth!')
 
     #return notification if the file type is wrong
-    return render_template('wrongfile.html')
+        return render_template('wrongfile.html')
 
 
 if __name__ == "__main__":
